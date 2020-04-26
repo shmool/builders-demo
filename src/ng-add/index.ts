8601ecdfd563db7dcb87a761831b7064dd18c8d2
shmool <shmuelaj@gmail.com>
2020-04-26 17:23:23 +0300

    Rename schematic to ng-add

--- src/ng-add/index.ts
+++ src/ng-add/index.ts
@@ -3,7 +3,7 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function angularCliBuildersDemo(_options: any): Rule {
+export function ngAdd(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
     return tree;
   };
--- src/ng-add/index_spec.ts
+++ src/ng-add/index_spec.ts
@@ -6,10 +6,10 @@ import * as path from 'path';
 const collectionPath = path.join(__dirname, '../collection.json');
 
 
-describe('angular-cli-builders-demo', () => {
+describe('ng-add', () => {
   it('works', () => {
     const runner = new SchematicTestRunner('schematics', collectionPath);
-    const tree = runner.runSchematic('angular-cli-builders-demo', {}, Tree.empty());
+    const tree = runner.runSchematic('ng-add', {}, Tree.empty());
 
     expect(tree.files).toEqual([]);
   });
